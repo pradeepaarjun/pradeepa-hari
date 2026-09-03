0cc2459f9cfacfd28565ac0725b1256e62fb2e11
Pradeepa Kujulva Arjunan <34764165+pradeepaarjun@users.noreply.github.com>
2026-09-03 18:50:29 +0200

    Initial commit

---
+++ script.js
@@ -0,0 +1,12 @@
+// =========================================
+// LANGUAGE BUTTON
+// =========================================
+
+const languageButton = document.getElementById("languageButton");
+
+languageButton.addEventListener("click", () => {
+    alert(
+        "French version coming soon! 🇫🇷\n\n" +
+        "We'll make the entire website bilingual."
+    );
+});
